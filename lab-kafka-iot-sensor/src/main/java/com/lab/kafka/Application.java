3e4fc4e90203040c58d7e0039aa3b43ff31eb0f0
Itallo <italloazevedo@hotmail.com>
2023-12-21 18:24:38 -0300

    fix: proxyTargetClass set to true

--- lab-kafka-iot-sensor/src/main/java/com/lab/kafka/Application.java
+++ lab-kafka-iot-sensor/src/main/java/com/lab/kafka/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 @EnableFeignClients
 @SpringBootApplication
 public class Application {
